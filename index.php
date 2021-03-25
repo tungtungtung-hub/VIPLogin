0707329bac7a72946a5811394aa7cc1ccefc8086
XuanTung <XuanTung@DESKTOP-MPKHJ93>
2021-03-25 13:17:28 +0700

    Login Form Simple - Username Input form

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form - VIP</title>
+</head>
+
+<body>
+    <div class="form-group">
+        <label for="username">Username</label>
+        <input class="form-control" placeholder="Username" name="username" type="text">
+    </div>
+</body>
+
+</html>
